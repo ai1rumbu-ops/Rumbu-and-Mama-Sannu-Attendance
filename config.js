3a6902cb58217444d81385f375925d861e1c4c9a
ai1rumbu-ops <ai1.rumbu@rumbuindustries.com>
2026-09-04 17:10:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Paste the Apps Script Web App URL here after deploying the backend.
 // It must end in /exec (not /dev).
 // Example: "https://script.google.com/macros/s/AKfycb.../exec"
-var API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+var API_URL = "https://script.google.com/macros/s/AKfycbxzJNol6FF5C-x_zO8nYxToV4f3k7AMDgAXLktvE6Ih0WTvaQWED3AyGjZKPW_A2gEYbg/exec";
